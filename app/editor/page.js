c5400d2793559cb72be83a2ce6714bdfb23e149b
Utkarsh Singh Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-04-19 08:55:24 +0530

    editor length

--- app/editor/page.js
+++ app/editor/page.js
@@ -1,4 +1,4 @@
 export default function Page(){
-    return <iframe width="90%" height="80%" className="mx-auto mt-10" src={"https://mindsprint-practice-backend.onrender.com/editor"} allowfullscreen></iframe>
+    return <iframe width="90%" height="768px" className="mx-auto mt-10" src={"https://mindsprint-practice-backend.onrender.com/editor"} allowfullscreen></iframe>
     
 }
